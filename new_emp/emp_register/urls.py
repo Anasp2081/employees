7be56f6d6a35e37f4d2758fb1f922a53527149f8
Anasp2081 <anasp2081@gmail.com>
2025-03-26 09:55:04 +0530

    Compleated project

--- new_emp/emp_register/urls.py
+++ new_emp/emp_register/urls.py
@@ -3,6 +3,8 @@ from .import views
 
 urlpatterns = [
 
-    path('',views.employee_form),
-    path('list/',views.employee_list)
+    path('',views.employee_form, name='emp_insert'), #get and post req. for insert operation
+    path('<int:id>/',views.employee_form, name='emp_update'), # get and post req. for update operation
+    path('<int:id>',views.employee_delete, name='emp_delete'),
+    path('list/',views.employee_list, name='emp_list') # get req. retrieve and display all records
 ]
